2f2f92306c8bffe7183b12b1ce871c4cefea1982
Alvaro.Barrado <Alvaro.Barrado@live.u-tad.com>
2018-10-12 20:15:10 +0200

    Pantallas 1 y 2 diseñadas

---
+++ app/src/main/java/com/utad/idiomakids/PersonalData.java
@@ -0,0 +1,13 @@
+package com.utad.idiomakids;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_data);
+    }
+}
