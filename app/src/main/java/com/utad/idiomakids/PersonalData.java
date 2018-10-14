8e5bcbac935e3c77ccdaea0b729f780842443589
Alvaro.Barrado <Alvaro.Barrado@live.u-tad.com>
2018-10-14 16:06:08 +0200

    Pantallas 1, 2, 3 y 4 diseñadas (4 falta Intent)

--- app/src/main/java/com/utad/idiomakids/PersonalData.java
+++ app/src/main/java/com/utad/idiomakids/PersonalData.java
@@ -1,7 +1,9 @@
 package com.utad.idiomakids;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalData extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class PersonalData extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_data);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent (view.getContext(), Profile.class);
+        startActivityForResult(intent, 0);
     }
 }
