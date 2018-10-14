b7587c6dbb9124e560dfcfbc20fa2ad2ad818e91
Alvaro.Barrado <Alvaro.Barrado@live.u-tad.com>
2018-10-14 11:32:53 +0200

    Pantallas 1, 2 y 3 diseñadas

--- app/src/main/java/com/utad/idiomakids/FormData.java
+++ app/src/main/java/com/utad/idiomakids/FormData.java
@@ -1,7 +1,9 @@
 package com.utad.idiomakids;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FormData extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FormData extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_data);
 
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent (view.getContext(), PersonalData.class);
+        startActivityForResult(intent, 0);
     }
 }
